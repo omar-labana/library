a71949963cfb37cbd77bf7de8a053a6a86507596
Omar Labana <omarlabana@outlook.com>
2021-06-17 00:27:48 +0200

    Add basic UI rendering

--- app.js
+++ app.js
@@ -1,1 +1,76 @@
-console.log("Hello World!")
+const library = [];
+const booksDiv = document.getElementById('books');
+
+function Book(author, title, nop, read) {
+  this.author = author;
+  this.title = title;
+  this.nop = nop;
+  this.read = read;
+}
+
+library.push(new Book('The Odin Project', 'JavaScript', 101, false));
+library.push(new Book('Microverse', 'RoR', 42, true));
+library.push(new Book('Zoom', 'Meetings', 155, false));
+library.push(new Book('Clockify', 'Time', 23, true));
+
+const addBook = () => {
+
+};
+
+// UI Render
+
+const createCard = (book) => {
+  // ul
+  const ul = document.createElement('ul');
+  ul.className += ' border-2 p-6 rounded-lg';
+
+  // Author li
+  const author = document.createElement('li');
+  author.innerHTML = `Author: ${book.author}`;
+  ul.appendChild(author);
+
+  // Title li
+  const title = document.createElement('li');
+  title.innerHTML = `Title: ${book.title}`;
+  ul.appendChild(title);
+
+  // nop li
+  const nop = document.createElement('li');
+  nop.innerHTML = `Title: ${book.nop}`;
+  ul.appendChild(nop);
+
+  // read li
+  const read = document.createElement('li');
+  read.innerHTML = `Read: ${book.read ? 'Yes' : 'No'}`;
+  ul.appendChild(read);
+
+  // remove btn
+  let btn = document.createElement('button');
+  btn.innerHTML = 'Remove';
+  btn.className += 'bg-red-700 p-2 border rounded text-white text-sm';
+  btn.addEventListener('click', () => {
+    console.log('Removed');
+  });
+  ul.appendChild(btn);
+
+  // read button
+  if (!book.read) {
+    btn = document.createElement('button');
+    btn.className += 'bg-blue-700 p-2 border rounded text-white text-sm';
+    btn.innerHTML = 'Mark as Read';
+    btn.addEventListener('click', () => {
+      console.log('Marked');
+    });
+    ul.appendChild(btn);
+  }
+
+  booksDiv.appendChild(ul);
+};
+
+const renderBooks = (books) => {
+  books.forEach((book) => {
+    createCard(book);
+  });
+};
+
+renderBooks(library);
